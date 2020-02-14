26556ff2ad80082d16115b80d6750f31bd0bc0da
mianyan <565857351@qq.com>
2020-02-14 15:30:07 +0800

    加入根据文件修改时间和文件大小判断文件是否被修改的功能

--- core.py
+++ core.py
@@ -1,8 +1,12 @@
+"""
+程序备份时所用内存与算hash时单次读的块大小有关，与线程最大数有关
+"""
 import os
 import json
 import shutil
 import hashlib
 from threading import Thread, Semaphore
+import filecmp
 
 
 def get_time():
@@ -47,13 +51,40 @@ def copy_symlink(src, dst):
     os.symlink(os.readlink(src), dst)
 
 
-def _create_file(src, dst, _new_md5_list, _all_md5_list, _max_thread_num):
+def _create_file_md5(src, dst, _new_md5_list, _all_md5_list, _max_thread_num):
     # print("\rcp:{}".format(src), end='', flush=True)
     # 判断文件名是否为软连接
     if os.path.islink(src):
         # 如果是，再建一个软连接
         copy_symlink(src, dst)
         # os.symlink(os.readlink(source_file_path), target_file_path)
+    else:
+        # 不是,就正常的算md5
+        file_md5 = get_hash(src)
+        if file_md5 not in _all_md5_list:
+            # Thread(target=shutil.copy2, args=[source_file_path, target_file_path]).start()
+            shutil.copy2(src, dst)
+            _new_md5_list.update({file_md5: dst})
+        else:
+            os.link(_all_md5_list[file_md5], dst)
+    _max_thread_num.release()
+
+
+def _create_file_md5pp(src, dst, _last_dst, _new_md5_list, _all_md5_list, _max_thread_num):
+    # print("\rcp:{}".format(src), end='', flush=True)
+    # 判断文件名是否为软连接
+    if os.path.islink(src):
+        # 如果是，再建一个软连接
+        copy_symlink(src, dst)
+        # os.symlink(os.readlink(source_file_path), target_file_path)
+    # 这里经过测试证明，and时会先算and前的，如果为false，就不会算and后的bool值了
+    # a = False
+    # a and b
+    # out: False
+    # b and a
+    # out: NameError: name 'd' is not defined
+    elif os.path.exists(_last_dst) and filecmp.cmp(src, _last_dst):
+        os.link(_last_dst, dst)
     else:
         # 不是,就正常的算md5
         file_md5 = get_hash(src)
@@ -81,15 +112,24 @@ def backup_core(src: str, dst: str, max_thread_num=256):
             # 加载json文件
             with open(target_path + '/md5_list.json') as fp:
                 final_md5_list = json.load(fp)
+                # 找出上一次的备份文件夹路径，方便对比文件是否修改
+                if len(final_md5_list) > 1:
+                    last_target_path = target_path + '/' + list(final_md5_list.keys())[-2]
+                elif len(final_md5_list) == 1:
+                    last_target_path = target_path + '/' + 'base'
+                else:
+                    last_target_path = None
             time_tag = time
         else:
             time_tag = 'base'
             final_md5_list = {'base': {}}
+            last_target_path = None
     else:
         # 如果不存在md5_list.json则创建
         open(target_path + '/md5_list.json', 'w').close()
         time_tag = 'base'
         final_md5_list = {'base': {}}
+        last_target_path = None
 
     target_path = target_path + '/' + time_tag
     os.makedirs(target_path)
@@ -103,39 +143,47 @@ def backup_core(src: str, dst: str, max_thread_num=256):
     new_md5_list = {}
     walk = os.walk(source_path)
 
-    for item in walk:
-        for folder in item[1]:
-            target_folder_path = target_path + item[0][len(source_path):] + '/' + folder
-            if not os.path.exists(target_folder_path):
-                os.makedirs(target_folder_path)
+    if last_target_path is None:
+        for item in walk:
+            for folder in item[1]:
+                target_folder_path = target_path + item[0][len(source_path):] + '/' + folder
+                if not os.path.exists(target_folder_path):
+                    os.makedirs(target_folder_path)
 
-        for file in item[2]:
-            # 得到原文件绝对路径
-            source_file_path = item[0] + '/' + file
-            # 得到目的文件绝对路径
-            target_file_path = target_path + item[0][len(source_path):] + '/' + file
-            """
-            # 判断文件名是否为软连接
-            if os.path.islink(source_file_path):
-                # 如果是，再建一个软连接
-                copy_symlink(source_file_path, target_file_path)
-                # os.symlink(os.readlink(source_file_path), target_file_path)
-            else:
-                # 不是,就正常的算md5
-                file_md5 = get_hash(source_file_path)
-                if file_md5 not in all_md5_list:
-                    # Thread(target=shutil.copy2, args=[source_file_path, target_file_path]).start()
-                    shutil.copy2(source_file_path, target_file_path)
-                    new_md5_list.update({file_md5: target_file_path})
-                else:
-                    os.link(all_md5_list[file_md5], target_file_path)
-            """
-            max_thread_num.acquire()
-            Thread(target=_create_file, args=[source_file_path,
-                                              target_file_path,
-                                              new_md5_list,
-                                              all_md5_list,
-                                              max_thread_num]).start()
+            for file in item[2]:
+                # 得到原文件绝对路径
+                source_file_path = item[0] + '/' + file
+                # 得到目的文件绝对路径
+                target_file_path = target_path + item[0][len(source_path):] + '/' + file
+
+                max_thread_num.acquire()
+                Thread(target=_create_file_md5, args=[source_file_path,
+                                                      target_file_path,
+                                                      new_md5_list,
+                                                      all_md5_list,
+                                                      max_thread_num]).start()
+
+    else:
+        for item in walk:
+            for folder in item[1]:
+                target_folder_path = target_path + item[0][len(source_path):] + '/' + folder
+                if not os.path.exists(target_folder_path):
+                    os.makedirs(target_folder_path)
+
+            for file in item[2]:
+                # 得到原文件绝对路径
+                source_file_path = item[0] + '/' + file
+                # 得到目的文件绝对路径
+                target_file_path = target_path + item[0][len(source_path):] + '/' + file
+                last_target_file_path = last_target_path + item[0][len(source_path):] + '/' + file
+
+                max_thread_num.acquire()
+                Thread(target=_create_file_md5pp, args=[source_file_path,
+                                                        target_file_path,
+                                                        last_target_file_path,
+                                                        new_md5_list,
+                                                        all_md5_list,
+                                                        max_thread_num]).start()
 
     final_md5_list.update({time_tag: new_md5_list})
     md5_list_str = json.dumps(final_md5_list, sort_keys=True, indent=4)
@@ -145,4 +193,4 @@ def backup_core(src: str, dst: str, max_thread_num=256):
 
 
 if __name__ == '__main__':
-    backup_core('~', '/media/mianyan/linux_backup/home')
+    backup_core('~/anaconda3', '/media/mianyan/linux_backup/anaconda3')
